beac40be43cdb771819add131ece14e409b80485
Taylor Monacelli <taylormonacelli@gmail.com>
2024-03-24 14:41:34 -0700

    Working

---
+++ README.md
@@ -0,0 +1,9 @@
+# downpaint
+
+## Purpose
+
+Learn the canonical way to organize ansible collections by following the molecule getting started guide from https://ansible.readthedocs.io/projects/molecule/getting-started/.
+
+## Motivation
+
+I'd like to use ansible to setup elastic and elastic certs from https://www.elastic.co/guide/en/elasticsearch/reference/current/configuring-stack-security.html.
---
+++ molecule_test.sh
@@ -0,0 +1,116 @@
+#!/usr/bin/env bash
+
+set -e
+set -x
+set -u
+
+dir=$(mktemp -d /tmp/test.XXXXX)/faris
+mkdir -p $dir
+cd $dir
+
+rm -rf /tmp/o1
+
+
+echo $dir >/tmp/o1
+
+
+rye init
+
+rm -rf src
+
+rye pin 3.12
+
+rye add molecule
+
+rye sync --quiet
+
+set +x
+. .venv/bin/activate
+set -x
+
+python --version
+
+ansible --version
+molecule --version
+mkdir -p $dir/collections/ansible_collections
+cd $dir/collections/ansible_collections
+
+# create new collection bar in namespace foo
+ansible-galaxy collection init foo.bar 
+
+
+cd $dir/collections/ansible_collections/foo/bar/roles
+ansible-galaxy role init my_role
+cat my_role/tasks/main.yml
+cat >my_role/tasks/main.yml<<'EOF'
+---
+- name: Task is running from within the role
+  ansible.builtin.debug:
+    msg: "This is a task from my_role."
+EOF
+
+
+mkdir -p $dir/collections/ansible_collections/foo/bar/playbooks
+cd $dir/collections/ansible_collections/foo/bar/playbooks
+
+cat >my_playbook.yml<<'EOF'
+---
+- name: Test new role from within this playbook
+  hosts: localhost
+  gather_facts: false
+  tasks:
+    - name: Testing role
+      ansible.builtin.include_role:
+        name: foo.bar.my_role
+        tasks_from: main.yml
+EOF
+
+mkdir -p $dir/collections/ansible_collections/foo/bar/extensions
+cd $dir/collections/ansible_collections/foo/bar/extensions
+
+# https://ansible.readthedocs.io/projects/molecule/getting-started/
+# Adding Molecule to the Collection
+
+molecule init scenario
+
+# https://ansible.readthedocs.io/projects/molecule/getting-started/#run-a-full-test-sequence
+# molecule test --destroy=never
+molecule test
+
+
+cd $dir/collections/ansible_collections/foo/bar/extensions
+cat >molecule/default/converge.yml <<EOF
+---
+- name: Include a role from a collection
+  hosts: localhost
+  gather_facts: false
+  tasks:
+    - name: Testing role
+      ansible.builtin.include_role:
+        name: foo.bar.my_role
+        tasks_from: main.yml
+EOF
+
+cat molecule/default/converge.yml
+
+cd $dir/collections/ansible_collections/foo/bar/extensions
+molecule converge
+molecule test
+
+
+cat >molecule/default/converge.yml <<EOF
+---
+- name: Include a playbook from a collection
+  ansible.builtin.import_playbook: foo.bar.my_playbook
+EOF
+
+cat molecule/default/converge.yml
+
+
+cd $dir/collections/ansible_collections/foo/bar/extensions
+molecule converge
+molecule test
+
+cd $dir
+tree --charset=ascii $dir | tee tree.txt
+rg --files $dir | tee list.txt
